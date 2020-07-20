4adc6562a21b211a6bfe5305275dcbc4d700afb3
shanthi <kshanthima@gmail.com>
2020-07-20 11:12:46 +0100

    Added Project

---
+++ EATest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EATest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        string url = "https://www.confirmtkt.com/";
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ EATestProject/Class1.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace EAAutoFramework
+{    public class Class1
+    {
+    }
+}
